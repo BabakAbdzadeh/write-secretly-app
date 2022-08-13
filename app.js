15383153bdde52f72a4dc3cbc3187fcdc0ffb373
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-08-13 04:48:00 +0200

    Encrypted PW saving

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //jshint esversion:6
+//  ---------------- requirments    ------------
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
 
 const app = express();
 
@@ -11,8 +14,28 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+ //  --------------- DB part  ---------------------
+
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptionFields: ['password']});
+
+const User = new mongoose.model('user', userSchema);
 
 
+
+
+
+
+
+//  ----------------- App ----------------------
 app
 .get("/", (req, res)=>{
   res.render("home");
@@ -22,6 +45,37 @@ app
 })
 .get("/register", (req, res)=>{
   res.render("register");
+})
+.post("/register", (req, res)=>{
+  const newUser = new User({
+    email : req.body.username ,
+    password: req.body.password
+  });
+
+  newUser.save((err)=>{
+    if(!err){
+      res.render("secrets");
+    }else{
+      console.log(err);
+    };
+  });
+})
+.post("/login",(req, res)=>{
+  const user ={
+    username : req.body.username,
+    password: req.body.password
+  };
+
+  User.findOne({email: user['username']}, (err, foundUser)=>{
+    if(!err){
+      if(foundUser){
+        res.render("secrets");
+      }
+    }else{
+      console.log(err);
+    };
+  });
+
 });
 
 
