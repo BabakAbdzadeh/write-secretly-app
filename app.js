bce2d9b232636691539a9e7e94e81a9004730246
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-09-05 16:15:12 +0200

    Level 5 cookies & sessions

--- app.js
+++ app.js
@@ -3,94 +3,131 @@
 require('dotenv').config();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const bcrypt = require('bcrypt');
 
- // --------- framework initialization  ----------------------
+// requires for Cookies and session, no need to require passport-local
+const session = require('express-session');
+const passport = require('passport');
+const passortLocalMongoose = require("passport-local-mongoose");
+
+
+// --------- framework initialization  ----------------------
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(express.static("public"))
+  .set('view engine', 'ejs')
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+
+  // initialization for express-session and passport libraries:
+  .use(session({
+    secret: "desired string!",
+    // Forces the session to be saved back to the session store, even if the session was never modified during the request.
+    resave: false,
+    // Forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified.
+    saveUninitialized: true,
+    cookie: {
+      // HTTPS is necessary for secure cookies
+      secure: false
+    }
+  }))
+  .use(passport.initialize())
+  .use(passport.session());
 
 //  ------------------ initialization variables ----------------
-const saltRounds = 10;
 
 
- //  --------------- DB initialization  ------------------------
+
+//  --------------- DB initialization  ------------------------
 
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+// for Authentication porpuse:
+userSchema.plugin(passortLocalMongoose);
 
 const User = new mongoose.model('user', userSchema);
 
+// for Auth porpuse:
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //  ------------------------ Application   ------------------------------
 app
-.get("/", (req, res)=>{
-  res.render("home");
-})
-.get("/login", (req, res)=>{
-  res.render("login");
-})
-.get("/register", (req, res)=>{
-  res.render("register");
-})
-.post("/register", (req, res)=>{
-
-  // Hashing and Salting
-  bcrypt.hash(req.body.password, saltRounds, function(err,hash){
-    const newUser = new User({
-      email : req.body.username,
-      password: hash
-    });
-      console.log(newUser);
-    newUser.save((err)=>{
-      if(!err){
-        res.render("secrets");
-      }else{
-        console.log(err);
-      };
-    });
+  .get("/", (req, res) => {
+    res.render("home");
   })
-})
-.post("/login",(req, res)=>{
-
-  const loginUser ={
-    username : req.body.username,
-    password: req.body.password
-  };
-  User.findOne({email: loginUser['username']}, (err, foundUser)=>{
-    if(!err){
-
-      console.log(foundUser);
-      if(foundUser){
-
-        bcrypt.compare(loginUser['password'], foundUser['password'], (err, result)=>{
-          if(result === true){
-  
-            res.render("secrets");
-          };
+  .get("/login", (req, res) => {
+    res.render("login");
+  })
+  .get("/register", (req, res) => {
+    res.render("register");
+  })
+  .get("/logout", (req, res) => {
+    // from passport library
+    req.logout((err) => {
+      if (err) console.log(err);
+    });
+    res.redirect("/");
+  })
+  .get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+      res.render("secrets");
+    } else {
+      res.redirect("/login");
+    }
+  })
+  .post("/register", (req, res) => {
+    User.register({
+      username: req.body.username,
+    }, req.body.password, function(err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register")
+      }
+      // passport.authenticate() is middleware which will authenticate the request.
+      // In the route down here, the 'local' strategy is used to verify a username and password. passport-local library
+      else {
+        const authenticate = passport.authenticate('local');
+        authenticate(req, res, () => {
+          res.redirect("/secrets");
         });
-      };
-    }else{
-      console.log(err);
-    };
+      }
+    });
+
+  })
+  .post("/login", (req, res) => {
+
+    const user = new User({
+      password: req.body.password,
+      username: req.body.username
+    });
+
+    // this method comes from passport:
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate('local')(req, res, function() {
+          res.redirect("/secrets");
+        })
+      }
+    })
+
   });
 
-});
 
 
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port: 3000");
 })
